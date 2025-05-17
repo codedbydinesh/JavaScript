98c9521c42aca50462ea4441b776176eface8797
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-05-17 20:40:40 +0530

    script updated

--- BackgroudColorChanger/script.js
+++ BackgroudColorChanger/script.js
@@ -4,8 +4,6 @@ const body = document.querySelector("body")
 btn.forEach(function(buttons){
     console.log(buttons);
     buttons.addEventListener('click', function(e){
-        console.log(e);
-        console.log(e.target);
         if(e.target.id === 'green'){
             body.style.backgroundColor = e.target.id;
         }else if(e.target.id === 'blue'){
